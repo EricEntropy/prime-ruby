f9b24cb6b70e74d285c53d3a4aaad714f86a8e49
Eric <contreras.eric18@gmail.com>
2021-01-18 01:10:26 -0500

    commit tree f6bb6e356ae5e066e2badda834aed598d59781ab

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(input) 
+
+    #WORRY ABOUT THE CORRECT RANGES / BOUNDARIES like negatives, zero and 1 which will 
+    #make mod not work 
+    if input <= 1
+        return false
+    end
+    
+    i = 2
+    while i < input 
+        # not prime
+         if input % i == 0 
+            return false
+         end
+         #increment outside if 
+         i += 1
+    end 
+    return true   
+end 
+
